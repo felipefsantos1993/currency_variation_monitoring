9a254484467be1fea7e02838939b5210e56b7395
Felipe Firmino Santos <felipefsantos.93@gmail.com>
2025-01-04 22:00:10 -0300

    Update code.py

--- code.py
+++ code.py
@@ -9,4 +9,9 @@ headers = {
 response = requests.get(url, headers=headers)
 data = response.json
 df = pd.json_normalize(data())
-print(df)
+usd_brl = float(df["USDBRL.high"][0])
+print(usd_brl)
+
+brl = float(input("How much money (R$) do you have?\n"))
+convertion = brl / usd_brl
+print("With R${:.2f} you can buy ${:.2f} currently".format(brl, convertion))
