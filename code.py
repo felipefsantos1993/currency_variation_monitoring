71eec4fd7dea9013a59e3a788dbf35276ced6b7c
Felipe Firmino Santos <felipefsantos.93@gmail.com>
2025-01-05 22:35:26 -0300

    Update code.py

--- code.py
+++ code.py
@@ -14,4 +14,5 @@ print(usd_brl)
 
 brl = float(input("How much money (R$) do you have?\n"))
 convertion = brl / usd_brl
+print("The current cotation from USD to BRL is R${:.2f}".format(convertion))
 print("With R${:.2f} you can buy ${:.2f} currently".format(brl, convertion))
